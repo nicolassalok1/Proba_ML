085b091fc0ffc5dd58ff5264986e5f3cba30f70a
salok1 <nicolas93pelletier@gmail.com>
2025-05-09 15:39:25 +0200

    all fine j'ai l'impression

--- setup.ps1
+++ setup.ps1
@@ -8,6 +8,7 @@ $ErrorActionPreference = "Stop"
 Write-Host "Installation des dépendances Python..."
 #
 conda env create -f environment.yml -y
+conda activate Proba_ML
 pip install -r requirements.txt -y
 
 
